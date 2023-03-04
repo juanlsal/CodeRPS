2ec66aa212098d42f144f135d3bae128ef4ad5b5
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-03-03 21:39:50 -0600

    updated the playground with crucial information about the game

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -1,2 +1,46 @@
-﻿//Playground
-//add
+﻿/* 
+ 
+Title: 
+Code RPS
+  
+  
+  
+Decription: 
+In this game, you'll face off against the computer in a classic game of rock paper scissors, but with a twist: you'll be testing your knowledge of basic programming concepts!
+To start, you'll be prompted to choose your weapon of choice: rock, paper or scissors. Once you've made your choice, the computer will randomly select its own weapon and the
+two choices will be compared to determine the winner. if you're not familiar with the rules of rock paper scissors, here they are:
+
+   * Rock beats scissors
+   * Scissors beats paper
+   * Paper beats rock
+         
+If you win, you'll be rewarded with a point and the opportunity to answer a question testing your coding skills and knowledge of basic programming concepts, if you answer the
+question correctly you will be rewarded an additional point, but if you answer question incorrectly your opponent will have an opportunity to answer the question, if they answer
+the question correctly they will be rewarded with a point. To add an extra challenge, you can set a time limit for each question. if you don't answer the question within the
+time, you'll forfeit the round. There are 6 different opponents you can choose to challenge each one more skilled then the other. Think you can take all of them on? Give it a try
+and see if you beat the computer!
+
+
+
+Components the game musting have:
+1. A way of select your weapon.
+2. A way of randomly selecting which weapon the computer selects.
+3. A way of allowing the player to select the difficulty of the computer.
+4. A way of making the computer more challenging.
+5. A way of winning a game of rps to reward a point to the winner.
+6. A way of the winner answering a question after each game of rps.
+7. A way of randomly selecting the question.
+8. A way of answering the question.
+9. A way of answering the question correctly will reward an additional point.
+10. A way of giving the question to the other if answered wrong.
+11.A way to give a point to whoever stole the question and answered correctly.
+12. A way of creating a loop so after each question is done it returns to a game of rps.
+13. A way of making an updating scoreboard.
+14. A way of selecting what amount of points must be reached in order to win the game.
+15. A way of selecting a time limit for answering the questions.
+
+*/
+
+
+
+
