8e36137e903d8a32fbbd6b5a5a8a91ac264e910c
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-03-06 03:34:20 -0600

    functional scoreboard, still needs works on visual

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -3,154 +3,145 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //Title
-        Console.WriteLine("Rock Paper Sciccors: The Game \nCreated by: Juan Lopez Salamanca \n\n");
-
-        //Data Collection
+        Console.WriteLine("Rock Paper Scissors: The Game \nCreated by: Juan Lopez Salamanca \n\n");
         Console.WriteLine("Player, please type your name.");
         string player1 = Console.ReadLine();
         Console.WriteLine("\n");
         Console.WriteLine("Welcome to RPS: The Game\n");
-
-        //Scoreboard
-
+        Console.WriteLine("Lets play!\nremember...\nr = Rock\np = Paper\ns = Scissors\n\nSelect Attack Then Press Enter!\n");
         int score1 = 0;
         int score2 = 0;
-
-
-        //instructions
-        Console.WriteLine("Lets play!\nremember...\nr = Rock\np = Paper\ns = Sciccors\n\nSelect Attack Then Press Enter!\n");
-
-        //varaibles for user
-        string Rock = "r";
-        string Paper = "p";
-        string Sciccors = "s";
-        string attack = Console.ReadLine();
-
-        Console.WriteLine("\n");
-
-        //user selection
+        while (score1 < 5)
         {
-            if (attack == Rock)
+            string attack = Console.ReadLine();
+            switch (attack)
             {
-                Console.WriteLine("You have selected... ROCK");
+                case "r":
+                    Console.WriteLine("You have selected... ROCK");
+                    break;
+                case "p":
+                    Console.WriteLine("You have selected... PAPER");
+                    break;
+                case "s":
+                    Console.WriteLine("You have selected... SCISSORS");
+                    break;
+                default:
+                    Console.WriteLine("Try Again");
+                    break;
             }
-            else if (attack == Paper)
+            string enemyrock = "ROCK";
+            string enemypaper = "PAPER";
+            string enemysciccors = "SCISSORS";
+            string[] selectableStrings = new string[3] { enemyrock, enemypaper, enemysciccors };
+            Random rand = new Random();
+            string Eattack = selectableStrings[rand.Next(0, selectableStrings.Length)];
+            Console.WriteLine("your Opponent selected... " + Eattack);
+            switch (attack)
             {
-                Console.WriteLine("You have selected... PAPER");
-            }
-            else if (attack == Sciccors)
-            {
-                Console.WriteLine("You have selected... SCICCORS");
-            }
-            else
-            {
-                Console.WriteLine("Try Again");
-            }
-
-        }
-
-        //varaibles for opponent
-        string enemyrock = "ROCK";
-        string enemypaper = "PAPER";
-        string enemysciccors = "SCISSORS";
-        string[] selectableStrings = new string[3] { enemyrock, enemypaper, enemysciccors };
-        Random rand = new Random();
-        string randomValue = selectableStrings[rand.Next(0, selectableStrings.Length)];
-
-
-        Console.WriteLine("your Opponent selected... " + randomValue);
-
-
-        //opponent selection/ results/ scorboard update
-        {
-            if (attack == Rock)
-            {
-                {
-                    if (randomValue == enemyrock)
+                case "r":
                     {
-                        Console.WriteLine("\nYou have DRAWN\n\n");
-                        Console.WriteLine("SCORE");
-                        Console.WriteLine(player1 + ": " + score1);
-                        Console.WriteLine("Opponent: " + score2 + "\n");
-
+                        switch (Eattack)
+                        {
+                            case "ROCK":
+                                {
+                                    Console.WriteLine("tied");
+                                    Console.WriteLine("Scoreboard");
+                                    Console.WriteLine(player1 + " : " + score1);
+                                    Console.WriteLine("Computer: " + score2);
+                                    break;
+                                }
+                            case "PAPER":
+                                {
+                                    Console.WriteLine("lost");
+                                    score2++;
+                                    Console.WriteLine("Scoreboard");
+                                    Console.WriteLine(player1 + " : " + score1);
+                                    Console.WriteLine("Computer: " + score2);
+                                    break;
+                                }
+                            case "SCISSORS":
+                                {
+                                    Console.WriteLine("won");
+                                    score1++;
+                                    Console.WriteLine("Scoreboard");
+                                    Console.WriteLine(player1 + " : " + score1);
+                                    Console.WriteLine("Computer: " + score2);
+                                    break;
+                                }
+                        }
+                        break;
                     }
-                    else if (randomValue == enemypaper)
+                case "p":
                     {
-                        Console.WriteLine("\nYou have LOST\n\n");
-                        Console.WriteLine("SCORE");
-                        score2++;
-                        Console.WriteLine(player1 + ": " + score1);
-                        Console.WriteLine("Opponent: " + score2 + "\n");
-
+                        switch (Eattack)
+                        {
+                            case "ROCK":
+                                {
+                                    Console.WriteLine("won");
+                                    score2++;
+                                    Console.WriteLine("Scoreboard");
+                                    Console.WriteLine(player1 + " : " + score1);
+                                    Console.WriteLine("Computer: " + score2);
+                                    break;
+                                }
+                            case "PAPER":
+                                {
+                                    Console.WriteLine("tied");
+                                    Console.WriteLine("Scoreboard");
+                                    Console.WriteLine(player1 + " : " + score1);
+                                    Console.WriteLine("Computer: " + score2);
+                                    break;
+                                }
+                            case "SCISSORS":
+                                {
+                                    Console.WriteLine("lost");
+                                    score1++;
+                                    Console.WriteLine("Scoreboard");
+                                    Console.WriteLine(player1 + " : " + score1);
+                                    Console.WriteLine("Computer: " + score2);
+                                    break;
+                                }
+                        }
+                        break;
                     }
-                    else if (randomValue == enemysciccors)
+                case "s":
                     {
-                        Console.WriteLine("\nYou have WON\n\n");
-                        Console.WriteLine("SCORE");
-                        score1++;
-                        Console.WriteLine(player1 + ": " + score1);
-                        Console.WriteLine("Opponent: " + score2 + "\n");
+                        {
+                            switch (Eattack)
+                            {
+                                case "ROCK":
+                                    {
+                                        Console.WriteLine("lost");
+                                        score2++;
+                                        Console.WriteLine("Scoreboard");
+                                        Console.WriteLine(player1 + " : " + score1);
+                                        Console.WriteLine("Computer: " + score2);
+                                        break;
+                                    }
+                                case "PAPER":
+                                    {
+                                        Console.WriteLine("won");
+                                        score1++;
+                                        Console.WriteLine("Scoreboard");
+                                        Console.WriteLine(player1 + " : " + score1);
+                                        Console.WriteLine("Computer: " + score2);
+                                        break;
+                                    }
+                                case "SCISSORS":
+                                    {
+                                        Console.WriteLine("tied");
+                                        Console.WriteLine("Scoreboard");
+                                        Console.WriteLine(player1 + " : " + score1);
+                                        Console.WriteLine("Computer: " + score2);
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
                     }
-
-                }
-            }
-            else if (attack == Paper)
-            {
-                {
-                    if (randomValue == enemyrock)
-                    {
-                        Console.WriteLine("\nYou have WON\n\n");
-                        Console.WriteLine("SCORE");
-                        score1++;
-                        Console.WriteLine(player1 + ": " + score1);
-                        Console.WriteLine("Opponent: " + score2 + "\n");
-                    }
-                    else if (randomValue == enemypaper)
-                    {
-                        Console.WriteLine("\nYou have DRAWN\n\n");
-                        Console.WriteLine("SCORE");
-                        Console.WriteLine(player1 + ": " + score1);
-                        Console.WriteLine("Opponent: " + score2 + "\n");
-                    }
-                    else if (randomValue == enemysciccors)
-                    {
-                        Console.WriteLine("\nYou have LOST\n\n");
-                        Console.WriteLine("SCORE");
-                        score2++;
-                        Console.WriteLine(player1 + ": " + score1);
-                        Console.WriteLine("Opponent: " + score2 + "\n");
-                    }
-
-                }
-            }
-            else if (attack == Sciccors)
-            {
-                {
-                    if (randomValue == enemyrock)
-                    {
-                        Console.WriteLine("\nYou have LOST\n\n");
-                        Console.WriteLine("SCORE");
-                        score2++;
-                        Console.WriteLine(player1 + ": " + score1);
-                        Console.WriteLine("Opponent: " + score2 + "\n");
-                    }
-                    else if (randomValue == enemypaper)
-                    {
-                        Console.WriteLine("\nYou have WON\n\n");
-                        Console.WriteLine("SCORE");
-                        score1++;
-                        Console.WriteLine(player1 + ": " + score1);
-                        Console.WriteLine("Opponent: " + score2 + "\n");
-                    }
-                    else if (randomValue == enemysciccors)
-                    {
-                        Console.WriteLine("\nYou have DRAWN\n\n");
-                        Console.WriteLine("SCORE");
-                        Console.WriteLine(player1 + ": " + score1);
-                        Console.WriteLine("Opponent: " + score2 + "\n");
-                    }
-                }
+                default:
+                    Console.WriteLine("hey");
+                    break;
             }
         }
     }
